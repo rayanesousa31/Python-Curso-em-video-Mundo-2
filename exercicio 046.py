410c18598da1fb489b5fe1880851809ee840106a
Rayane Sousa <eng.rayanesousa@gmail.com>
2022-02-14 09:12:04 -0300

    Upload exercício 046 - Mundo 2

---
+++ exercicio 046.py
@@ -0,0 +1,8 @@
+#Faça um programa que mostre na tela uma contagem regressiva para o estouro
+#de fogos de artificio, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+import time
+for contagem in range (10,-1,-1):
+    print(contagem),time.sleep(1)
+
+print('🎉🎉Feliz ano novo 🎉🎉')
