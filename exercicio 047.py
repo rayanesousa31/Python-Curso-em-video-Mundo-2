5615def63003c832c4860c3bd55258763fc185aa
Rayane Sousa <eng.rayanesousa@gmail.com>
2022-02-14 09:14:01 -0300

    Upload exercício 047 - Mundo 2

---
+++ exercicio 047.py
@@ -0,0 +1,10 @@
+#Crie um programa que mostre na tela todos os 
+# numeros pares que estão no intervalo 
+#entre 1 e 50.
+
+for n in range (2, 51,2):
+    #if n % 2 == 0:
+       print(n, end='.')
+    
+
+
